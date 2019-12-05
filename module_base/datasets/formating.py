b253caa1fd39e1ce227c55d01f13d23bc145521e
hejian <flystarhe@qq.com>
2019-12-05 18:06:45 +0800

    module_base/datasets/formating.py - new

---
+++ module_base/datasets/formating.py
@@ -0,0 +1,33 @@
+import torch
+import numpy as np
+from .registry import PIPELINES
+
+
+def to_tensor(data):
+    if isinstance(data, torch.Tensor):
+        return data
+    elif isinstance(data, np.ndarray):
+        return torch.from_numpy(data)
+    elif isinstance(data, (list, tuple)):
+        return torch.tensor(data)
+    elif isinstance(data, int):
+        return torch.LongTensor([data])
+    elif isinstance(data, float):
+        return torch.FloatTensor([data])
+    else:
+        raise TypeError('type {} cannot be converted to tensor.'.format(type(data)))
+
+
+@PIPELINES.register_module
+class ToTensor(object):
+
+    def __init__(self, keys):
+        self.keys = keys
+
+    def __call__(self, results):
+        for key in self.keys:
+            results[key] = to_tensor(results[key])
+        return results
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(keys={})'.format(self.keys)
--- module_base/datasets/transforms.py
+++ module_base/datasets/transforms.py
@@ -1,4 +1,3 @@
-import torch
 import numpy as np
 from .registry import PIPELINES
 
@@ -8,36 +7,6 @@ sitk ndarray: (depth, height, width)
 numpy ndarray: (height, width, channel)
 torch tensor shape: (channel, height, width)
 '''
-
-
-def to_tensor(data):
-    if isinstance(data, torch.Tensor):
-        return data
-    elif isinstance(data, np.ndarray):
-        return torch.from_numpy(data)
-    elif isinstance(data, (list, tuple)):
-        return torch.tensor(data)
-    elif isinstance(data, int):
-        return torch.LongTensor([data])
-    elif isinstance(data, float):
-        return torch.FloatTensor([data])
-    else:
-        raise TypeError('type {} cannot be converted to tensor.'.format(type(data)))
-
-
-@PIPELINES.register_module
-class ToTensor(object):
-
-    def __init__(self, keys):
-        self.keys = keys
-
-    def __call__(self, results):
-        for key in self.keys:
-            results[key] = to_tensor(results[key])
-        return results
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(keys={})'.format(self.keys)
 
 
 @PIPELINES.register_module
