e6216e526c8fbb0780d4f525d50c40f534742377
hejian <flystarhe@qq.com>
2019-12-13 16:51:17 +0800

    configs/single_cnn_r9_motion.py

---
+++ configs/single_cnn_r9_motion.py
@@ -0,0 +1,40 @@
+# model settings
+model = dict(
+    type='ResNet',
+    depth=9,
+    in_channels=1,
+    out_channels=256,
+    padding_type='reflect',
+    drop_prob=0.0
+)
+# model training and testing settings
+train_cfg = dict()
+test_cfg = dict()
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+train_pipeline = [
+    dict(type='LoadDicomFromFile'),
+]
+test_pipeline = [
+    dict(type='LoadDicomFromFile'),
+]
+data = dict(
+    imgs_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file='coco_train.json',
+        data_root=data_root,
+        pipeline=train_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file='coco_test.json',
+        data_root=data_root,
+        pipeline=test_pipeline)
+)
+# optimizer
+
+# learning policy
+
+# runtime settings
